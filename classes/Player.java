2ec524e26b153a164490cf9524485956b703e2fb
Jarrettgohh <jarrettgoh.xz@gmail.com>
2025-01-22 00:16:11 +0800

    -

--- classes/Player.java
+++ classes/Player.java
@@ -5,6 +5,7 @@ public class Player {
 	boolean handedness; // eg. 0 -> right, 1 -> left
 	int height;
 	boolean category; // eg. 0 -> singles, 1 -> doubles
+	
 
 	public void smash() {}
     public void drop() {}
